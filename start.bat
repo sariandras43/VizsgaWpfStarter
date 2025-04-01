597dac7c3f30b726c7e111fdc59e3c55ca3816b6
sariandras43 <sariandras43@gmail.com>
2025-04-01 09:12:10 +0200

    Updated start.bat: parametrized dbname

--- start.bat
+++ start.bat
@@ -1,6 +1,13 @@
+@echo off
+set /p dbName="Add meg az adatbazis nevet: "
+
+set connectionString=Server=localhost;Database=%dbName%;Uid=root;Pwd=;
+
+dotnet tool install --global dotnet-ef --version 8.0.11
 dotnet add package Microsoft.EntityFrameworkCore -v 8.0.11
 dotnet add package Microsoft.EntityFrameworkCore.Design -v 8.0.11
 dotnet add package Microsoft.EntityFrameworkCore.Tools -v 8.0.11
 dotnet add package MySql.EntityFrameworkCore -v 8.0.11
 
-dotnet ef dbcontext scaffold "Server=localhost;Database=dbname;Uid=root;Pwd=;" MySql.EntityFrameworkCore --output-dir Data
+dotnet ef dbcontext scaffold "%connectionString%" MySql.EntityFrameworkCore --output-dir Data
+echo A scaffold parancs lefutott.
