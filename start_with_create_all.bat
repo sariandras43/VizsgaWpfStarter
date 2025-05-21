b433cc0351094458f2dae05cbedf2b3f4ae43ab1
sariandras43 <sariandras43@gmail.com>
2025-05-21 19:46:21 +0200

    Added bypass to PS commands, updated XAML files

--- start_with_create_all.bat
+++ start_with_create_all.bat
@@ -1,16 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
-set /p projectName="Add meg a konzolos projekt nevet: "
+set /p consoleProjectName="Add meg a konzolos projekt nevet: "
+set /p desktopProjectName="Add meg az asztali projekt nevet: "
 set /p dbName="Add meg az adatbazis nevet: "
 
-dotnet new console -n %projectName%
-dotnet new wpf -n "%projectName%GUI"
-cd %projectName%
-dotnet new sln -n %projectName% 
-dotnet sln %projectName%.sln add %projectName%.csproj
-dotnet sln %projectName%.sln add "..\%projectName%GUI\%projectName%GUI.csproj"
-cd "..\%projectName%GUI"
-echo A WPF és konzolos projekt letrejott es hozza lett adva a solutionhoz.
+dotnet new console -n %consoleProjectName%
+dotnet new wpf -n %desktopProjectName%
+cd %consoleProjectName%
+dotnet new sln -n %consoleProjectName% 
+dotnet sln %consoleProjectName%.sln add %consoleProjectName%.csproj
+dotnet sln %consoleProjectName%.sln add "..\%desktopProjectName%\%desktopProjectName%.csproj"
+cd "..\%desktopProjectName%"
+echo A WPF es konzolos projekt letrejott es hozza lett adva a solutionhoz.
 
 dotnet add package Microsoft.EntityFrameworkCore -v 8.0.11
 dotnet add package Microsoft.EntityFrameworkCore.Design -v 8.0.11
@@ -45,18 +46,18 @@ if exist "..\Database.cs" (
 )
 
 echo A namespacek cserelese folyamatban a MainWindow.xaml fajlban...
-powershell -Command "(Get-Content 'MainWindow.xaml') -replace 'VizsgaWpfStarter.MainWindow', '%projectName%GUI.MainWindow' | Set-Content 'MainWindow.xaml' -Encoding utf8"
-powershell -Command "(Get-Content 'MainWindow.xaml') -replace 'clr-namespace:VizsgaWpfStarter', 'clr-namespace:%projectName%GUI' | Set-Content 'MainWindow.xaml' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'MainWindow.xaml') -replace 'VizsgaWpfStarter.MainWindow', '%desktopProjectName%.MainWindow' | Set-Content 'MainWindow.xaml' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'MainWindow.xaml') -replace 'clr-namespace:VizsgaWpfStarter', 'clr-namespace:%desktopProjectName%' | Set-Content 'MainWindow.xaml' -Encoding utf8"
 echo A namespacek cserelese sikeres volt a MainWindow.xaml fajlban.
 
 
 
 echo A namespacek, importok cserelese folyamatban a MainWindow.xaml.cs fajlban...
-powershell -Command "(Get-Content 'MainWindow.xaml.cs') -replace 'VizsgaWpfStarter', '%projectName%GUI' | Set-Content 'MainWindow.xaml.cs' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'MainWindow.xaml.cs') -replace 'VizsgaWpfStarter', '%desktopProjectName%' | Set-Content 'MainWindow.xaml.cs' -Encoding utf8"
 echo A namespacek cserelese sikeres volt a MainWindow.xaml.cs fajlban.
 
 echo A namespacek, importok cserelese folyamatban a Database.cs fajlban...
-powershell -Command "(Get-Content 'Database.cs') -replace 'VizsgaWpfStarter', '%projectName%GUI' | Set-Content 'Database.cs' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'Database.cs') -replace 'VizsgaWpfStarter', '%desktopProjectName%' | Set-Content 'Database.cs' -Encoding utf8"
 echo A namespacek cserelese sikeres volt a Database.cs fajlban.
 
 set "firstChar=!dbName:~0,1!"
@@ -68,11 +69,11 @@ if not defined upperFirst set "upperFirst=%firstChar%"
 set "dbUpper=!upperFirst!!dbName:~1!"
 
 echo A DbContextek cserelese folyamatban a Database.cs fajlban...
-powershell -Command "(Get-Content 'Database.cs') -replace 'new AppContext', 'new %dbUpper%Context' | Set-Content 'Database.cs' -Encoding utf8"
-powershell -Command "(Get-Content 'Database.cs') -replace 'static AppContext', 'static %dbUpper%Context' | Set-Content 'Database.cs' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'Database.cs') -replace 'new AppContext', 'new %dbUpper%Context' | Set-Content 'Database.cs' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'Database.cs') -replace 'static AppContext', 'static %dbUpper%Context' | Set-Content 'Database.cs' -Encoding utf8"
 echo A DbContextek cserelese sikeres volt a Database.cs fajlban.
 
 echo A DbContextek cserelese folyamatban a MainWindow.xaml.cs fajlban...
-powershell -Command "(Get-Content 'MainWindow.xaml.cs') -replace 'private AppContext', 'private %dbUpper%Context' | Set-Content 'MainWindow.xaml.cs' -Encoding utf8"
+powershell -ep Bypass -Command "(Get-Content 'MainWindow.xaml.cs') -replace 'private AppContext', 'private %dbUpper%Context' | Set-Content 'MainWindow.xaml.cs' -Encoding utf8"
 
 cd ..
